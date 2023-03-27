325db76edc1d1c7566be890db08fe2da758b1e4a
daotoan-hd <daotoan.hd@gmail.com>
2023-03-27 11:19:45 +0700

    Update

--- .github/actions/javascript-sample/main.js
+++ .github/actions/javascript-sample/main.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 
 
@@ -13,6 +14,7 @@ function run() {
         // Get the JSON webhook payload for the event that triggered the workflow
         const payload = JSON.stringify(github.context.payload, undefined, 2)
         console.log(`The event payload: ${payload}`);
+        exec.exec("whoami")
     } catch (error) {
         core.setFailed(error.message);
     }
