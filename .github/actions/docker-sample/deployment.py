e5b20ffd7a41dfe09ef21192855aaed21dd40085
daotoan-hd <daotoan.hd@gmail.com>
2023-03-27 13:32:17 +0700

    Update

--- .github/actions/docker-sample/deployment.py
+++ .github/actions/docker-sample/deployment.py
@@ -2,8 +2,10 @@ import os
 import mimetypes
 
 def run():
-    print(os.environ)
+    # print(os.environ)
     who_to_greet = os.environ['INPUT_WHO-TO-GREET']
+    print("Welcome {}!".format(who_to_greet))
+    os.system("output=\"This is sample output\" >> $GITHUB_OUTPUT")
     
 
 if __name__ == '__main__':
